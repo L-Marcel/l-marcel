51a6fd5c2a3fcdb3442e2371032b073dc8d2a02b
l-marcel <lmgh1312@gmail.com>
2023-01-16 20:27:14 -0300

    style: changing warning section

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -2,7 +2,7 @@ interface Props {}
 
 export default function Projects({}: Props) {
   return (
-    <section className="relative mx-12 mt-14 flex flex-row items-center justify-start gap-4 md:mx-16 md:mt-[5rem]">
+    <section className="relative mx-12 mt-14 flex flex-col items-center justify-start gap-4 md:mx-16 md:mt-[5rem]">
       <h1>Em manutenção...</h1>
       <p>Parece que excedi o limite do meu token do GitHub...</p>
     </section>
