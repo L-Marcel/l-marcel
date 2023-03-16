87217c8759e4b7db9df628436ab85b89c99182d0
l-marcel <lmgh1312@gmail.com>
2023-03-16 09:25:58 -0300

    fix: removing bug with path revalidations

--- src/utils/revalidatePath.ts
+++ src/utils/revalidatePath.ts
@@ -2,7 +2,7 @@ import { NextApiResponse } from "next";
 
 async function revalidatePath(res: NextApiResponse, path: string) {
   try {
-    await Promise.all([res.revalidate(`/en-us/${path}`), res.revalidate(`/${path}`)]);
+    await Promise.all([res.revalidate(`/en-US/${path}`), res.revalidate(`/${path}`)]);
   } catch (err) {
     console.log(err);
   }
